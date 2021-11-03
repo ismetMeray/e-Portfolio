7ad61b6d0f4df22be614c5a2777b446a544b5c6d
ismet <ismet-meray@hotmail.com>
2021-11-03 18:01:39 +0100

    adding platform imags

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -31,6 +31,14 @@ export class HomeComponent implements OnInit {
       .getGameList(sort, search).subscribe((gameList: APIResponse<Game>) => {
         this.games = gameList.results;
         console.log(gameList);
+        for(var i = 0; i < this.games.length; ++i){
+            console.log(this.games[i]);
+            for(var j = 0; j < this.games[i].parent_platforms.length; ++j){
+              console.log(this.games[i].parent_platforms[j].platform.name);
+
+            }
+        }
+
       }
       );
 
