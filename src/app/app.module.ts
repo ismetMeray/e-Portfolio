837ec6f0e2ba144950f9de31b36aede7df917cdd
ismetMeray <ij.meray@student.avans.nl>
2021-11-03 13:50:42 +0100

    init project, retrieve api game

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule, HttpErrorResponse, HTTP_INTERCEPTORS } from '@angular/common/http'
 import { GaugeModule } from 'angular-gauge';
 import { MatTabsModule } from '@angular/material/tabs'
 import { MatIconModule } from '@angular/material/icon'
@@ -13,6 +13,8 @@ import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatSelectModule } from '@angular/material/select';
 import { SearchbarComponent } from './components/searchbar/searchbar.component';
 import { HomeComponent } from './components/home/home.component'
+import { HttpHeadersInterceptor } from './interceptors/htpp-params.interceptor';
+import { HttpErrorsInterceptor } from './interceptors/http-errors.interceptor';
 
 @NgModule({
   declarations: [
@@ -32,7 +34,18 @@ import { HomeComponent } from './components/home/home.component'
     MatFormFieldModule,
     MatSelectModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpHeadersInterceptor,
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpErrorsInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/interceptors/htpp-params.interceptor.ts
+++ src/app/interceptors/htpp-params.interceptor.ts
@@ -1,20 +1,33 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable, throwError as observableThrowError } from "rxjs";
-import { catchError } from 'rxjs/operator';
+import { Observable } from "rxjs";
 
 @Injectable()
-export class HttpErrorsInterceptor implements HttpInterceptor {
-    constructor() {
+export class HttpHeadersInterceptor implements HttpInterceptor {
+  constructor() {}
 
-    }
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        throw next.handle(req).pipe(
-            catchError((err) => {
-                console.log(err);
-                return observableThrowError(err);
-            })
-        );
-    }
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    req = req.clone({
+      setHeaders: {
+        'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
+        'x-rapidapi-key': '8548c0be52msh65d5fca7d5d5f20p161982jsncd7ce087223d'
+      },
+      setParams: {
+        key: '3cebbe6d8842453883ec184b78852d8f',
+      }
+    });
+    return next.handle(req);
+  }
 }
 
+var options = {
+  method: 'GET',
+  url: 'https://rawg-video-games-database.p.rapidapi.com/games',
+  headers: {
+    'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
+    'x-rapidapi-key': '8548c0be52msh65d5fca7d5d5f20p161982jsncd7ce087223d'
+  }
+};
